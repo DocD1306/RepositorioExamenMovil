3ac983a4cd577ec0791698d8ecf2691d219a5433
Diego <diegofjda2@gmail.com>
2025-11-16 16:38:05 +0100

    Guardar

--- app/src/main/java/net/iessochoa/diegovalencia/repositorioexamenmovil/listasscrolleables/ListaScrolleable.kt
+++ app/src/main/java/net/iessochoa/diegovalencia/repositorioexamenmovil/listasscrolleables/ListaScrolleable.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +25,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.iessochoa.diegovalencia.repositorioexamenmovil.R
+import net.iessochoa.diegovalencia.repositorioexamenmovil.listasscrolleables.datos.Datasource
+import net.iessochoa.diegovalencia.repositorioexamenmovil.listasscrolleables.modelo.Affirmation
 
 
 @Composable
