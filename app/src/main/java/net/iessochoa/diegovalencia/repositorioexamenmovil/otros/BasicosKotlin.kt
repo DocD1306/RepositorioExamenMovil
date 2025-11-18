92436da90b15934d90fc39cac3f405fee5a830c8
Diego <diegofjda2@gmail.com>
2025-11-18 08:37:19 +0100

    Guardar

---
+++ app/src/main/java/net/iessochoa/diegovalencia/repositorioexamenmovil/otros/BasicosKotlin.kt
@@ -0,0 +1,87 @@
+package net.iessochoa.diegovalencia.repositorioexamenmovil.otros
+
+// Basic Kotlin features summarized in one code block
+
+// --- Variables ---
+/*
+val immutableValue = 42
+var mutableValue = "Hello"
+
+// --- Functions ---
+fun greet(name: String): String {
+    return "Hi, $name!"
+}
+
+// --- Data Class ---
+data class User(val id: Int, val name: String)
+
+// --- Lists ---
+val immutableList = listOf("Apple", "Banana", "Cherry")
+val mutableList = mutableListOf(1, 2, 3)
+val arrayList = arrayListOf("Kotlin", "Java", "C++")
+
+// --- Adding / Removing elements ---
+mutableList.add(4)
+mutableList.remove(2)
+arrayList.add("Python")
+
+// --- Iterating over lists ---
+for (item in immutableList) {
+    println(item)
+}
+
+immutableList.forEach {
+    println("Item: $it")
+}
+
+for (i in mutableList.indices) {
+    println("Index $i -> ${mutableList[i]}")
+}
+
+// --- Conditional structures ---
+val number = 10
+if (number > 5) {
+    println("Greater than 5")
+} else {
+    println("5 or less")
+}
+
+// --- When (switch-like) ---
+when (number) {
+    1 -> println("One")
+    2, 3 -> println("Two or Three")
+    in 4..10 -> println("Between 4 and 10")
+    else -> println("Something else")
+}
+
+// --- Loops ---
+for (i in 1..5) println("Range: $i")
+for (i in 0 until 5) println("Until: $i")
+for (i in 10 downTo 1 step 2) println("Down: $i")
+
+var counter = 0
+while (counter < 3) {
+    println("While loop: $counter")
+    counter++
+}
+
+// --- Null safety ---
+var nullableText: String? = null
+println(nullableText?.length)       // Safe call
+println(nullableText ?: "No text")  // Elvis operator
+
+// --- Classes ---
+open class Animal {
+    open fun sound() = "Some sound"
+}
+
+class Dog : Animal() {
+    override fun sound() = "Woof!"
+}
+
+// --- Using objects ---
+val user = User(1, "Alice")
+println(user)
+println(Dog().sound())
+
+*/
